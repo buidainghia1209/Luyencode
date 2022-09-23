5a78d98d24d434e6522bdc860338bd7bab2a429a
buidainghia1209 <91354969+buidainghia1209@users.noreply.github.com>
2022-09-23 13:44:06 +0700

    Create VT12.cpp

---
+++ VT12.cpp
@@ -0,0 +1,30 @@
+using System;
+
+namespace baivt10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            string s = Console.ReadLine();
+            var s1 = s.Split(' ');
+            for(int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(s1[i]);
+            }
+            int tg;
+            //sapxep theo giam dan
+            for (int i = 0; i < n-1; i++)
+                for (int j = i + 1;j<n;j++)
+                    if (a[i] > a[j])
+                    {
+                        tg = a[i];
+                        a[i] = a[j];
+                        a[j] = tg;
+                    }
+                Console.Write("{0} ", a[n-1]-a[0]);
+        }
+    }
+}
